d2c42bad8d020a2951d949e4e2bc40485b40ff8c
Wu Long <wulong.hugle@gmail.com>
2015-04-11 19:46:36 +0800

    add heroku site

--- public/javascripts/Config.js
+++ public/javascripts/Config.js
@@ -3,6 +3,6 @@ var Config = {
     WIDTH : 1000,
     PORT : 3000,
     FRAME_RATE : 40,
-    SERVER_NAME : "localhost"
-    //SERVER_NAME : "localhost"
+    SERVER_NAME : "captainhook.herokuapp.com",
+    //SERVER_NAME : "172.28.180.43"
 };
